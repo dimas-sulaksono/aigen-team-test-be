03cbfec8d14f06abf477cc95d158dc44681ac6bb
Muhammad Ma'ruf <maruuf82@gmail.com>
2025-03-10 17:49:51 +0700

    Read detail payments 1. Read all payments 2. Default sort all payment by last created_at 3. Implement pagination using query params with customizable size 10, 30, 50 per page using query params

---
+++ src/main/java/com/schoolpayment/team/controller/PaymentTypeController.java
@@ -0,0 +1,58 @@
+package com.schoolpayment.team.controller;
+
+import com.schoolpayment.team.dto.response.ApiResponse;
+import com.schoolpayment.team.model.PaymentType;
+import com.schoolpayment.team.security.CustomUserDetails;
+import com.schoolpayment.team.service.PaymentTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/payment-type")
+public class PaymentTypeController {
+
+    @Autowired
+    private PaymentTypeService paymentTypeService;
+
+    // add payment type
+    @PostMapping("/add")
+    public ResponseEntity<?> addPaymentType(@RequestParam String name) {
+
+        paymentTypeService.cratePaymentType(name);
+        return ResponseEntity.ok(new ApiResponse<>(200, "success add payment type"));
+    }
+
+    // update payment type
+    @PutMapping("/update")
+    public ResponseEntity<?> updatePaymentType(@RequestParam Long id, @RequestParam String name) {
+        paymentTypeService.updatePaymentType(id, name);
+        return ResponseEntity.ok(new ApiResponse<>(200, "success update payment type"));
+    }
+
+    // delete payment type
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deletePaymentType(@RequestParam Long id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        paymentTypeService.deletePaymentType(id);
+        return ResponseEntity.ok(new ApiResponse<>(200, "success delete payment type"));
+    }
+
+    // get all payment type
+    @GetMapping
+    public ResponseEntity<?> getAllPaymentType(@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "5") int size) {
+        Page<PaymentType> payments = paymentTypeService.getAllPaymentType(page, size);
+        return ResponseEntity.ok(new ApiResponse<>(200, payments));
+
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<?> searchPaymentType(@RequestParam String name, @RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "5") int size) {
+        Page<PaymentType> payments = paymentTypeService.searchPaymentType(name, page, size);
+        return ResponseEntity.ok(new ApiResponse<>(200, payments));
+    }
+
+
+
+}
---
+++ src/main/java/com/schoolpayment/team/dto/response/PaymentResponse.java
@@ -0,0 +1,44 @@
+package com.schoolpayment.team.dto.response;
+
+import com.schoolpayment.team.model.PaymentType;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class PaymentResponse {
+    private Long id;
+    private String name;
+    private UserPayment user = new UserPayment();
+    private StudentPayment student = new StudentPayment();
+    private PaymentType type;
+    private BigDecimal amount;
+    private String status;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+
+    public void setUser(Long id, String name) {
+        user.setId(id);
+        user.setName(name);
+    }
+
+    public void setStudent(String nis, String name) {
+        student.setNis(nis);
+        student.setName(name);
+    }
+}
+
+@Data
+class UserPayment {
+    private Long id;
+    private String name;
+}
+
+@Data
+class StudentPayment {
+    private String nis;
+    private String name;
+}
